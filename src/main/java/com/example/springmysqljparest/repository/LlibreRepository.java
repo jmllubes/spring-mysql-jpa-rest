2870d65b6fbbc9351c50ce6d9f74d7f0cfdf3369
Josep Maria Llubes Cliville <jmllubes@gmail.com>
2023-12-14 09:19:41 +0100

    repository llibre

---
+++ src/main/java/com/example/springmysqljparest/repository/LlibreRepository.java
@@ -0,0 +1,7 @@
+package com.example.springmysqljparest.repository;
+
+import com.example.springmysqljparest.model.Llibre;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LlibreRepository extends JpaRepository<Llibre, String> {
+}
