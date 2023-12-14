a98beb3c50a07a58779bf96efc462eab9f4a3711
Josep Maria Llubes Cliville <jmllubes@gmail.com>
2023-12-14 09:03:36 +0100

    Initial commit

---
+++ src/main/java/com/example/springmysqljparest/SpringMysqlJpaRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmysqljparest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMysqlJpaRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMysqlJpaRestApplication.class, args);
+    }
+
+}
